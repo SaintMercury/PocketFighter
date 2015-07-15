fa8e469653051d9eb8ea140f7ad7722688cd5bc2
Max Henzerling <Max.Henzerling@USSCOMHE1-L.LYV.LiveNation.com>
2015-07-15 14:31:30 -0700

    Dev environment is mostly functional. Testing IO/Express now

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,24 @@
 //Modules to add to the server file. WARNING: Do not add these to the top of every file. Requirements need to only be here.
-var expressio = require('express.io'),
-	fs = require('fs'),
-	url = require('url');
 
-//Do stuff here
+//Probably should not touch this. Was a pain to set up.
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//Serves the index.html and allows for the necessary client serving.
+app.use('/', express.static(__dirname + '/'));
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+//I can touch this
+io.on('connection', function(socket) {
+	//Add event handlers and emitters here. This is where it get cray
+	console.log('Someone is here');
+	socket.emit('connected', {data: "So proud of you"});
+});
+
+http.listen(1337, function() {
+	console.log('Running...');
+});
