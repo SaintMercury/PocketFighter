7a0d489f7501d1bc0c25f9dc11602ae40c2e3a2d
Max Henzerling <Max.Henzerling@USSCOMHE1-L.LYV.LiveNation.com>
2015-07-21 10:11:28 -0700

    Prototype of current ideology, going to implement game states

--- client/game/game.js
+++ client/game/game.js
@@ -1,83 +1,123 @@
+'use-strict';
+
 var socket = require('./socket.js');
 var controls = require('./controls/controls.js');
 
-var game;
-game = new Phaser.Game(800, 600, Phaser.Auto, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(800, 600, Phaser.Auto, '', {preload: preload, create: create, update: update});
 
 //Needed globals for game
 var players;
 var self;
 var others;
 var keys;
+var myId;
+var hostBool;
 
 //Initialize
 function preload() {
-	//pre setup
-	console.log(game.load.image('pixel','/Assets/Textures/pixel.png'));
-
-	//Assign event recievers here.
-	socket.on('connected', function(data) {
-		joinHandler(data);
-	});
-	socket.on('clientUpdate', function(data) {
-		clientUpdate(data);
-	});
-	socket.on('userDisconnect', function(data) {
-		leaveHandler(data);
-	})
+	others = {};
+	//pre setup - Load up assets, set event recievers, stuff like that nature
+	game.load.image('pixel','/Assets/Textures/pixel.png');
+	recieverSetup();
 }
 
 function create() {
+	//Create step - Create objects using loaded assets, set placement, stuff that's not 'really' active at runtime
 	players = game.add.group();
 	self = players.create(game.world.randomX, game.world.randomY, 'pixel');
 	self.scale.setTo(20,20);
 	self.anchor.setTo(0.5,0.5); //I think this centers the object's reference
 	self.tint = 0xff0000;
 
-	others = {};
+	socket.emit('fetchGameData', {id: myId, x: self.x, y: self.y});
 
 	keys = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
-	controls(self,keys);
-	serverUpdate();
-}
-
-function clientUpdate(data) {
-	var bud;
-	if(!!others[data.socketid] === false) {
-		bud = others[data.socketid] = players.create(data.x, data.y, 'pixel');
-		bud.scale.setTo(20,20);
-		bud.anchor.setTo(0.5,0.5);
-		bud.tint = 0x00ff00;
-	} else {
-	 bud = others[data.socketid];
+	if(hostBool !== undefined && myId !== undefined) {
+		controls(self,keys);
+		if(hostBool === false) {
+			var data = {};
+			data.id = myId;
+			data.x = self.x;
+			data.y = self.y;
+			socket.emit('clientSend', data);
+		} else {
+			socket.emit('hostSend', sendHostInstance());
+		}
 	}
-	bud.x = data.x;
-	bud.y = data.y;
 }
 
-function serverUpdate() {
-	var data = {
-		socketid: socket.id,
-		x: self.x,
-		y: self.y
-	};
-	socket.emit('serverUpdate', data);
+function recieverSetup() {
+	socket.on('sendID', function(data) {
+		myId = data;
+		socket.on('hostAssign', function(data) {
+			hostBool = true;
+			console.log(hostBool);
+			socket.on('hostRecieve', function(data) {
+				var player = others[data.id];
+				console.log(player);
+				player.x = data.x;
+				player.y = data.y;
+			});
+			socket.on('fetchGameData', function(data) {
+				//When a player requests the current instance on joining, make them in the world.
+				others[data.id] = players.create(data.x, data.y, 'pixel');
+				others[data.id].scale.setTo(20,20);
+				others[data.id].anchor.setTo(0.5,0.5);
+				others[data.id].tint = 0x0000ff;
+				socket.emit('recieveGameData', sendHostInstance());
+			});
+		});
+
+		socket.on('clientAssign', function(data) {
+			hostBool = false;
+			console.log(hostBool);
+			socket.on('clientRecieve', function(data) {
+				recieveHostInstance();
+			});
+			socket.on('recieveGameData', function(data) {
+				for(var i in data) {
+					console.log(data[i].id);
+					console.log(myId);
+					if(data[i].id != myId) {
+						var currentPlayer = data[i];
+						others[currentPlayer.id] = players.create(currentPlayer.x, currentPlayer.y, 'pixel');
+						others[currentPlayer.id].scale.setTo(20,20);
+						others[currentPlayer.id].anchor.setTo(0.5,0.5);
+						others[currentPlayer.id].tint = 0x0000ff;
+					}
+				}
+			});
+		});
+	});
 }
 
-function joinHandler(data) {
-	var bud = players.create(game.world.randomX, game.world.randomY, 'pixel');
-	bud.scale.setTo(20,20);
-	bud.anchor.setTo(0.5,0.5);
-	bud.tint = 0x00ff00;
-	others[data.socketid] = bud;
+function sendHostInstance() {
+	var data = {};
+	for(var i in others) {
+		var currentPlayer = others[i];
+		data[currentPlayer.id] = {};
+		data[currentPlayer.id].id = currentPlayer.id;
+		data[currentPlayer.id].x = currentPlayer.x;
+		data[currentPlayer.id].y = currentPlayer.y;
+	}
+	data[myId] = {};
+	data[myId].id = myId;
+	data[myId].x = self.x;
+	data[myId].y = self.y;
+	return data;
 }
 
-function leaveHandler(data) {
-	others[data.socketid].destroy();
-	delete others[data.socketid];
+function recieveHostInstance(data) {
+	for(var i in data) {
+		var currentPlayer = data[i];
+		if(currentPlayer.id != myId) {
+			others[currentPlayer.id].x = currentPlayer.x;
+			others[currentPlayer.id].y = currentPlayer.y;
+		}
+	}
 }
 
 module.exports = game;
--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
+var io = require('socket.io')(http, {
+	pingTimeout: 10000,
+	pingInterval: 500
+});
 
 //Serves the index.html and allows for the necessary client serving.
 app.use(express.static(__dirname + '/../build'));
@@ -13,21 +15,71 @@ app.get(__dirname+'../build', function (req, res) {
 	res.sendFile(__dirname + '/build/index.html');
 });
 
-//I can touch this
+var rooms = [];
+function Room(id) {
+	this.id = id;
+	this.currentPlayers = [];
+	this.maxPlayers = 4;
+}
+
 io.on('connection', function(socket) {
-	//Add event handlers and emitters here. This is where it get cray
-	//Thes socket parameter is the specific connection.
-	//io is the whole server
-	console.log(socket.id);
-	socket.broadcast.emit('connected', {socketid: socket.id});
-	socket.on('serverUpdate', function(data) {
-		socket.broadcast.emit('clientUpdate', data);
+	var myID = guid();
+	socket.emit('sendID', myID);
+	var roomID = joinHandler(socket, myID);
+
+	//Game data event forwarder
+	socket.on('fetchGameData', function(data) {
+		socket.to(roomID).emit('fetchGameData', data);
 	});
-	socket.on('disconnect', function() {
-		socket.broadcast.emit('userDisconnect', {socketid: socket.id});
-	})
+	socket.on('recieveGameData', function(data) {
+		socket.to(roomID).emit('recieveGameData', data);
+	});
+
+	//Host-Client Pseudo setup/forwarding system
+	socket.on('clientSend', function(data) {
+		socket.to(roomID).emit('hostRecieve', data);
+	});
+	socket.on('hostSend', function(data) {
+		socket.to(roomID).emit('clientRecieve', data);
+	});
 });
 
 http.listen(1337, function() {
 	console.log('Running...');
 });
+
+function joinHandler(socket, GUID) {
+	if(rooms.length === 0) {
+		var newRoom = new Room(Date.now());
+		rooms.push(newRoom);
+		newRoom.currentPlayers.push('h'+GUID);
+		socket.emit('hostAssign', newRoom.id);
+		socket.join(newRoom.id);
+		return newRoom.id;
+	} else {
+		for(var i = 0; i < rooms.length; i++) {
+			if(rooms[i].currentPlayers.length < rooms[i].maxPlayers) {
+				rooms[i].currentPlayers.push(GUID);
+				socket.emit('clientAssign', rooms[i].id);
+				socket.join(rooms[i].id);
+				return rooms[i].id;
+			}
+		}
+	}
+	var newRoom = new Room(Date.now());
+	rooms.push(newRoom);
+	newRoom.currentPlayers.push('h'+GUID);
+	socket.emit('hostAssign', newRoom.id);
+	socket.join(newRoom.id);
+	return newRoom.id;
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
