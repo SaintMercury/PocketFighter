6171c52cea26abceb22873543a356e95e59107cf
Max Henzerling <Max.Henzerling@USSCOMHE1-L.LYV.LiveNation.com>
2015-07-22 13:04:08 -0700

    I may have done it

--- client/game/game.js
+++ client/game/game.js
@@ -1,83 +1,35 @@
 var socket = require('./socket.js');
 var controls = require('./controls/controls.js');
 
-var game;
-game = new Phaser.Game(800, 600, Phaser.Auto, '', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(800, 600, Phaser.Auto, '');
 
-//Needed globals for game
-var players;
-var self;
-var others;
-var keys;
+var ID;
+var peer;
+var conn;
 
-//Initialize
-function preload() {
-	//pre setup
-	console.log(game.load.image('pixel','/Assets/Textures/pixel.png'));
-
-	//Assign event recievers here.
-	socket.on('connected', function(data) {
-		joinHandler(data);
+socket.on('obtainID', function(data) {
+	ID = data;
+	peer = new Peer(data, {host: 'localhost', port: 1337});
+	peer.on('open', function(id) {
+		socket.emit('ReadyForBrokerage', ID);
 	});
-	socket.on('clientUpdate', function(data) {
-		clientUpdate(data);
+
+	//Host
+	peer.on('connection', function(conn) {
+		conn.on('open', function() {
+			conn.on('data', function(data) {
+				console.log('Recieved Data: ' + data);
+			});
+		});
 	});
-	socket.on('userDisconnect', function(data) {
-		leaveHandler(data);
-	})
-}
+});
 
-function create() {
-	players = game.add.group();
-	self = players.create(game.world.randomX, game.world.randomY, 'pixel');
-	self.scale.setTo(20,20);
-	self.anchor.setTo(0.5,0.5); //I think this centers the object's reference
-	self.tint = 0xff0000;
-
-	others = {};
-
-	keys = game.input.keyboard.createCursorKeys();
-}
-
-function update() {
-	controls(self,keys);
-	serverUpdate();
-}
-
-function clientUpdate(data) {
-	var bud;
-	if(!!others[data.socketid] === false) {
-		bud = others[data.socketid] = players.create(data.x, data.y, 'pixel');
-		bud.scale.setTo(20,20);
-		bud.anchor.setTo(0.5,0.5);
-		bud.tint = 0x00ff00;
-	} else {
-	 bud = others[data.socketid];
-	}
-	bud.x = data.x;
-	bud.y = data.y;
-}
-
-function serverUpdate() {
-	var data = {
-		socketid: socket.id,
-		x: self.x,
-		y: self.y
-	};
-	socket.emit('serverUpdate', data);
-}
-
-function joinHandler(data) {
-	var bud = players.create(game.world.randomX, game.world.randomY, 'pixel');
-	bud.scale.setTo(20,20);
-	bud.anchor.setTo(0.5,0.5);
-	bud.tint = 0x00ff00;
-	others[data.socketid] = bud;
-}
-
-function leaveHandler(data) {
-	others[data.socketid].destroy();
-	delete others[data.socketid];
-}
+//Client
+socket.on('Client', function(data) {
+	conn = peer.connect(data);
+	conn.on('open', function() {
+		conn.send('I am client!');
+	});
+});
 
 module.exports = game;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-//Modules to add to the server file. WARNING: Do not add these to the top of every file. Requirements need to only be here.
-
-//Probably should not touch this. Was a pain to set up.
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var peerServer = require('peer').ExpressPeerServer(http);
 
-
-//Serves the index.html and allows for the necessary client serving.
-app.use(express.static(__dirname + '/../build'));
+app.use(express.static(__dirname + '/../build'), peerServer);
 app.get(__dirname+'../build', function (req, res) {
 	res.sendFile(__dirname + '/build/index.html');
 });
 
-//I can touch this
+var waitingHosts = [];
+
 io.on('connection', function(socket) {
-	//Add event handlers and emitters here. This is where it get cray
-	//Thes socket parameter is the specific connection.
-	//io is the whole server
 	console.log(socket.id);
-	socket.broadcast.emit('connected', {socketid: socket.id});
-	socket.on('serverUpdate', function(data) {
-		socket.broadcast.emit('clientUpdate', data);
+	socket.emit('obtainID', socket.id);
+	socket.on('ReadyForBrokerage', function() {
+		if(waitingHosts.length === 0) {
+			waitingHosts.push(socket);
+		} else {
+			waitingHosts.shift().emit('Client', socket.id);
+		}
 	});
-	socket.on('disconnect', function() {
-		socket.broadcast.emit('userDisconnect', {socketid: socket.id});
-	})
+});
+
+http.on('connection', function(id) {
+	//do something?
+	console.log('peer connected');
 });
 
 http.listen(1337, function() {
